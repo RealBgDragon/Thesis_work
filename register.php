d380b8a3b1229f9fde726d004bf3de00fe3b9197
RealBgDragon <128041836+RealBgDragon@users.noreply.github.com>
2024-01-15 21:57:35 +0200

    Update register.php

--- register.php
+++ register.php
@@ -4,6 +4,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
