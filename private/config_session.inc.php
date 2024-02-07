68daf6231e0ed85be7ad9f675e47fc51e5e14c94
RealBgDragon <128041836+RealBgDragon@users.noreply.github.com>
2024-02-07 11:24:33 +0200

    Update config_session.inc.php

--- private/config_session.inc.php
+++ private/config_session.inc.php
@@ -6,7 +6,7 @@ ini_set("session.use_strict_mode", 1);
 session_set_cookie_params([
     'lifetime' => 1800,
     'path' => '/',
-    'secure' => false, //TODO change later
+    'secure' => true, //TODO change later
     'httponly' => true,
 ]);
 
